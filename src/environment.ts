00b9d2d4c9ecd7d2a07a49813e7bdb659fc54e1e
Darryl <digi9346x@gmail.com>
2023-04-21 02:37:33 +0800

    Add env and global variables

---
+++ src/environment.ts
@@ -0,0 +1,7 @@
+const env = { ...process.env };
+
+class Environment {
+    public static STATS_URL = env.REACT_APP_STATS_URL ?? 'https://vylantze-foundry-bucket.s3.ap-southeast-1.amazonaws.com/data/stats.json'
+}
+
+export default Environment
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Environment from '@/environment'
+import XHR from '@/scripts/xhr'
+
+declare global {
+  var environment: typeof Environment
+  var xhr: XHR
+}
+globalThis.environment = Environment;
+globalThis.xhr = new XHR();
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
