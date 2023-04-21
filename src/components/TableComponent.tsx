e90c08da0acc8500ed8c2a2edef7b1e5c0b7661c
Darryl <digi9346x@gmail.com>
2023-04-22 07:24:27 +0800

    Add scrolling for table. Handle mobile.

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -25,45 +25,47 @@ export default function TableComponent (props: IProps) {
   if (controller === undefined) return (<div />);
 
   return (
-    <Table
-      className={styles.table}
-      bordered
-    >
-      <thead>
-        <tr>
-          <th className={styles.header} />
-          {controller.stats.map((stat, index) => {
+    <div className={styles.container}>
+      <Table
+        className={styles.table}
+        bordered
+      >
+        <thead>
+          <tr>
+            <th className={styles.header} />
+            {controller.stats.map((stat, index) => {
+              return (
+                <th className={styles.header} key={`header-${index}`}>
+                  <Image
+                    src={stat.user.avatar ?? ''}
+                    alt={`${stat.user.name} avatar`}
+                    width={50}
+                    height={50}
+                    priority
+                  />
+                  <div>{stat.user.name}</div>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>      
+          {controller.templates.map((template, index) => {
+            const descriptionStyle = template.isNested ? `${styles.nested} ${styles.rowhead}` : styles.rowhead;
+            const tdStyle = template.isNested ? `${styles.nested} ${styles.content}` : styles.content;
             return (
-              <th className={styles.header} key={`header-${index}`}>
-                <Image
-                  src={stat.user.avatar ?? ''}
-                  alt={`${stat.user.name} avatar`}
-                  width={50}
-                  height={50}
-                  priority
-                />
-                <div>{stat.user.name}</div>
-              </th>
+              <tr  key={`row-${index}`}>
+                <td className={descriptionStyle}>{template.name}</td>
+                {template.values.map((value, subIndex) =>
+                  <td className={tdStyle} key={`row-${index}-td-${subIndex}`}>
+                    {value}
+                  </td>
+                )}
+              </tr>
             );
           })}
-        </tr>
-      </thead>
-      <tbody>      
-        {controller.templates.map((template, index) => {
-          const descriptionStyle = template.isNested ? `${styles.nested} ${styles.rowhead}` : styles.rowhead;
-          const tdStyle = template.isNested ? `${styles.nested} ${styles.content}` : styles.content;
-          return (
-            <tr  key={`row-${index}`}>
-              <td className={descriptionStyle}>{template.name}</td>
-              {template.values.map((value, subIndex) =>
-                <td className={tdStyle} key={`row-${index}-td-${subIndex}`}>
-                  {value}
-                </td>
-              )}
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+        </tbody>
+      </Table>
+    </div>
   )
 }
