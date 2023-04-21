6cbdfc733601d0902d72c20f9f01602463deed5c
Darryl <digi9346x@gmail.com>
2023-04-22 06:58:45 +0800

    Add a collapsible component

---
+++ src/components/CollapsibleComponent.tsx
@@ -0,0 +1,36 @@
+  import React, { useState, PropsWithChildren, ReactNode } from 'react';
+import { CaretDownFill, CaretUpFill } from 'react-bootstrap-icons';
+import styles from '@/styles/CollapsibleComponent.module.css';
+
+interface IProps {
+  title: string | ReactNode
+  startCollapsed?: boolean;
+}
+
+export default function CollapsibleComponent (props: PropsWithChildren<IProps>) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(!props.startCollapsed);
+  const toggleCollapse = function () {
+    setIsExpanded(!isExpanded);
+  }
+
+  return (
+    <div className={styles.collapsible}>
+      <div className={styles.divider}>
+        {props.title}
+        <div style={{ flex: 1 }} />
+        <div
+          className={styles.toggle}
+          onClick={toggleCollapse}
+        >
+          {
+            isExpanded ? 
+            <CaretDownFill /> :
+            <CaretUpFill />
+          }
+        </div>
+      </div>
+      <hr />
+      {isExpanded ? props.children : <div />}
+    </div>
+  )
+}
